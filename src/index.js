19c800068d2916906f38c2722c26abcb1cc1868d
Motkova Kateryna <sirenkoekaterina90@gmail.com>
2022-12-04 16:53:03 +0200

    add files

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import './css/styles.css';
+import pokemonCardTpl from './templates/pokemon-card.hbs';
+
+
+// const DEBOUNCE_DELAY = 300;
+
+
+fetch('https://pokeapi.co/api/v2/pokemon/2/')// пойти на адрес
+.then(response => { 
+    // console.log(response.json());
+    return response.json(); // распарсить ответ, возвращается промис
+})
+.then(pokemon => {
+    console.log(pokemon);
+    const markup = pokemonCardTpl(pokemon);
+    console.log(markup);
+})
+.catch(error => {
+    console.log(error);
+});
+
